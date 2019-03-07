d36f1a074a3afe1a62e6f2de72ece2849e3dd9e2
bobram22 <robertoramirez.d.j@gmail.com>
2019-03-06 18:25:20 -0800

    Custom array list (IP)

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -1,23 +1,31 @@
 package _01_Custom_ArrayList;
 
+import org.junit.experimental.theories.FromDataPoints;
+
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
+	T[] array;
+	int size;
 	
 	public ArrayList() {
+		array = (T[])new Object[100];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return array[loc];
 	}
 	
 	public void add(T val) {
-		
+		array[size] = val;
+		size ++;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		for (int i = 0; i < array.length; i++) {
+			
+		}
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
